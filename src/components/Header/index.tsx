26be94dc33582a00c0e1a9af694fc4ae7df5bf35
Joilson M S Lopes <joilsonmirandasl@hotmail.com>
2022-07-23 19:38:51 -0300

    Add Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,16 @@
-export default function Header() {
-  // TODO
-}
+import NextLink from 'next/link';
+import styles from './header.module.scss';
+
+export const Header = () => {
+  return (
+    <header className={styles.container}>
+      <div className={styles.content}>
+        <NextLink href="/">
+          <a>
+            <img src="/assets/logo.svg" alt="logo" />
+          </a>
+        </NextLink>
+      </div>
+    </header>
+  );
+};
